d13b0644708ce7fbab5c7513dfa033260293ca0a
O-79 <adiguptaxd@gmail.com>
2024-08-01 20:28:48 -0400

    update

---
+++ Edit.py
@@ -0,0 +1,13 @@
+import numpy as np
+from scipy.signal import butter, lfilter
+
+class Edit:
+    def EQUALIZER(DAT, SPL_RTE, DB_ADD, HZ_LOW, HZ_UPP):
+        FCT_ADD = 10 ** (DB_ADD / 20.0)
+        NYQ = 0.5 * SPL_RTE
+        LOW = HZ_LOW / NYQ
+        UPP = HZ_UPP / NYQ
+        b, a = butter(1, [LOW, UPP], btype='band')
+        y = lfilter(b, a, DAT)
+        DAT_ADD = y * FCT_ADD
+        return DAT_ADD
---
+++ Styles.py
@@ -0,0 +1,193 @@
+class Styles:
+    LGHT = """
+                QWidget {
+                    background-color: #E0E0E0;
+                    color: #000000;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #F0F0F0;
+                    border: 1px solid #D0D0D0;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #000000;
+                }
+                QLineEdit:focus {
+                    border: 1px solid #D0D0D0;
+                }
+                QTextEdit {
+                    background-color: #F0F0F0;
+                    border: 1px solid #D0D0D0;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #000000;
+                }
+                QPushButton {
+                    background-color: #2FB7E0;
+                    border: 2px solid #279ABA;
+                    color: #000000;
+                    padding: 4px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #279ABA;
+                }
+                QPushButton:pressed {
+                    background-color: #279ABA;
+                }
+                QPushButton:disabled {
+                    background-color: #C0C0C0;
+                    border: 2px solid #A0A0A0;
+                }
+            """
+
+    DARK =  """
+                QWidget {
+                    background-color: #303030;
+                    color: #FFFFFF;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #404040;
+                    border: 1px solid #202020;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFFFF;
+                }
+                QTextEdit {
+                    background-color: #404040;
+                    border: 1px solid #202020;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFFFF;
+                }
+                QPushButton {
+                    background-color: #4DDDA6;
+                    border: 2px solid #43BA8A;
+                    color: #000000;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #43BA8A;
+                }
+                QPushButton:pressed {
+                    background-color: #43BA8A;
+                }
+                QPushButton:disabled {
+                    background-color: #C0C0C0;
+                    border: 2px solid #A0A0A0;
+                }
+            """
+
+    NGHT =  """
+                QWidget {
+                    background-color: #202020;
+                    color: #E8E0FF;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #303030;
+                    border: 1px solid #101010;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #E8E0FF;
+                }
+                QTextEdit {
+                    background-color: #303030;
+                    border: 1px solid #101010;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #E8E0FF;
+                }
+                QPushButton {
+                    background-color: #6844C4;
+                    border: 2px solid #52379E;
+                    color: #FFFFFF;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #52379E;
+                }
+                QPushButton:pressed {
+                    background-color: #52379E;
+                }
+                QPushButton:disabled {
+                    background-color: #808080;
+                    border: 2px solid #606060;
+                }
+            """
+
+    MISC_0 =  """
+                QWidget {
+                    background-color: #303030;
+                    color: #FFFFFF;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #404040;
+                    border: 1px solid #202020;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFF88;
+                }
+                QTextEdit {
+                    background-color: #404040;
+                    border: 1px solid #202020;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFF88;
+                }
+                QPushButton {
+                    background-color: #DDCC00;
+                    border: 2px solid #B7A800;
+                    color: #000000;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #B7A800;
+                }
+                QPushButton:pressed {
+                    background-color: #B7A800;
+                }
+                QPushButton:disabled {
+                    background-color: #C0C0C0;
+                    border: 2px solid #A0A0A0;
+                }
+                QToolBar {
+                    background-color: #404040;
+                    border: 1px solid #202020;
+                    spacing: 8px;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFF88;
+                }
+                QToolButton {
+                    background-color: #DDCC00;
+                    border: 2px solid #B7A800;
+                    color: #000000;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QToolButton:hover {
+                    background-color: #B7A800;
+                }
+                QToolButton:pressed {
+                    background-color: #B7A800;
+                }
+                QToolButton:disabled {
+                    background-color: #C0C0C0;
+                    border: 2px solid #A0A0A0;
+                }
+            """
